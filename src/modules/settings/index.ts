222da707df554318cc3f6c65c5fe52401fe1c448
CypherX <672420+CypherX@users.noreply.github.com>
2025-09-20 20:32:35 -0400

    add setting to show/hide module

--- src/modules/settings/index.ts
+++ src/modules/settings/index.ts
@@ -188,6 +188,7 @@ Settings.add(new Setting<string>('gameDisplayStyle', 'Game display style',
     ],
     'standard3'));
 Settings.add(new BooleanSetting('showMuteButton', 'Show mute/unmute button', true));
+Settings.add(new BooleanSetting('showGoalTrackerModule', 'Show Goal Tracker module on main screen', true));
 
 // CSS variable settings
 Settings.add(new CssVariableSetting('locked', 'Locked Location', [], '#000000'));
