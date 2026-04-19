dcf32101ee55ae98285c449ff4067bf824181991
CypherX <672420+CypherX@users.noreply.github.com>
2026-04-19 12:54:18 -0400

    clean up

--- src/scripts/shop/VeteranShop.ts
+++ src/scripts/shop/VeteranShop.ts
@@ -6,7 +6,7 @@ interface VeteranUnlockConfig {
 }
 
 class VeteranShop extends Shop {
-    static list: Record<GameConstants.VeteranUnlock, VeteranUnlockConfig> = {
+    static unlockList: Record<GameConstants.VeteranUnlock, VeteranUnlockConfig> = {
         [GameConstants.VeteranUnlock.PokerusVirus]: {
             isUnlocked: ko.observable(false),
             checkFunction: (playerData, saveData) => {
@@ -30,31 +30,36 @@ class VeteranShop extends Shop {
         super(items, name, requirements, hideBeforeUnlocked);
     }
 
-    public static isUnlockAvailable(unlock: GameConstants.VeteranUnlock): boolean {
-        return this.list[unlock]?.isUnlocked() ?? false;
-    }
-
     public static initialize(): void {
         const saveKeys = Object.keys(localStorage).filter((k: string) => k.startsWith('save')).map((k: string) => k.replace(/^save/, ''));
         if (saveKeys.length <= 1) {
             return;
         }
 
+        const unlocks = Object.values(VeteranShop.unlockList);
         for (const saveKey of saveKeys) {
             try {
                 const saveData = JSON.parse(localStorage.getItem(`save${saveKey}`));
                 const playerData = JSON.parse(localStorage.getItem(`player${saveKey}`));
 
-                for (const thing of Object.values(VeteranShop.list)) {
-                    if (thing.isUnlocked()) {
+                for (const unlock of unlocks) {
+                    if (unlock.isUnlocked()) {
                         continue;
                     }
 
-                    thing.isUnlocked(thing.checkFunction(playerData, saveData));
+                    unlock.isUnlocked(unlock.checkFunction(playerData, saveData));
                 }
             } catch (e) {
-                console.error(e);
+                console.error(`Failed to initialize Veteran unlocks for save key ${saveKey}:`, e);
+            }
+
+            if (unlocks.every((unlock) => unlock.isUnlocked())) {
+                break;
             }
         }
+    }
+
+    public static isUnlockAvailable(unlock: GameConstants.VeteranUnlock): boolean {
+        return this.unlockList[unlock]?.isUnlocked() ?? false;
     }
 }
